b63888292744bf4aaa0fd93e0aab294e3b12faf0
Hasan <130398453+hasan28742@users.noreply.github.com>
2023-05-06 17:26:51 +0600

    Update Computer will guess the number.py

--- Computer will guess the number.py
+++ Computer will guess the number.py
@@ -2,21 +2,22 @@ import random
 
 
 def computer_guess():
-
-    low = int(input("Enter the Lowest limit: "))
-    high = int(input("Enter the Highest limit: "))
-    feedback = ''
-    while feedback != 'c':
-        if low != high:
-            guess = random.randint(low, high)
-        else:
-            guess = low  # or high
-        feedback = input(f'if {guess} is-High (H) Low (L) Correct (C) ?? ').lower()
-        if feedback == "h":
-            high = guess - 1
-        elif feedback == "l":
-            low = guess + 1
-    print(f'Computer guessed the right number {guess}')
-
+    try:
+        low = int(input("Enter the Lowest limit: "))
+        high = int(input("Enter the Highest limit: "))
+        feedback = ''
+        while feedback != 'c':
+            if low != high:
+                guess = random.randint(low, high)
+            else:
+                guess = low  # or high
+            feedback = input(f'if {guess} is-High (H) Low (L) Correct (C) ?? ').lower()
+            if feedback == "h":
+                high = guess - 1
+            elif feedback == "l":
+                low = guess + 1
+        print(f'Computer guessed the right number {guess}')
+      except ValueError:
+        print('Invalid input, please follow the input instruction correctly.')
 
 computer_guess()
